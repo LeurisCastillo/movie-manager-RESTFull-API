c4dafcfab9b789f657dbf2d317c9e2771b4b753e
LeurisCastillo <leurisa.delossantos@gmail.com>
2021-10-20 19:47:31 -0400

    class db context packages and dependencies added

---
+++ Movies RESTFULL API/Movie manager/Data/MoviesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_manager.Data
+{
+    public class MoviesDbContext : DbContext
+    {
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> option) : base(option)
+        {
+                
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
--- Movies RESTFULL API/Movie manager/Startup.cs
+++ Movies RESTFULL API/Movie manager/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Movie_manager.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +27,13 @@ namespace Movie_manager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<MoviesDbContext>(option => 
+                option.UseSqlServer(Configuration.GetConnectionString("MyConnection")));
+
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +50,8 @@ namespace Movie_manager
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
